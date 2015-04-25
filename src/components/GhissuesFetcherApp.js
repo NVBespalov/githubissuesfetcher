7e62b22888f2279f9fbfdda0e803ddc0b7ef60e0
Nikolay <nbespalov@icloud.com>
2015-04-25 23:41:01 +0500

    search field

--- src/components/GhissuesFetcherApp.js
+++ src/components/GhissuesFetcherApp.js
@@ -6,14 +6,11 @@ var React = require('react/addons');
 require('normalize.css');
 require('../styles/main.css');
 var Issues = require('./Issues');
+
 var GhissuesFetcherApp = React.createClass({
   render: function() {
     return (
-        <div class="row">
-            <div class="col-lg-12">
-                <Issues />
-            </div>
-        </div>
+        <Issues />
     );
   }
 });
--- src/components/Issues.js
+++ src/components/Issues.js
@@ -7,10 +7,23 @@ require('styles/Issues.less');
 var Issues = React.createClass({
     render: function () {
         return (
-            <form>
-                <input type="text" class="form-control" placeholder="Github user name" />
-            </form>
+            <div className="col-lg-12">
+                <form className="form-inline">
+                    <div className="form-group">
+                        <label>Имя пользователя</label>
+                        <input type="text" className="form-control" placeholder="NVBespalov" />
+                    </div>
+                    <div className="form-group">
+                        <label>Имя репозитория</label>
+                        <input type="email" className="form-control" placeholder="Имя репозитория" />
+                    </div>
+                    <button type="submit" className="btn btn-default" onClick={this.handleSubmit}>Поиск</button>
+                </form>
+            </div>
         );
+    },
+    handleSubmit: function(field, e) {
+
     }
 });
 
