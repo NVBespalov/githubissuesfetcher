b23ea029963e1ede9b6281ec83ce73d8bd1ceda7
Nick Bespalov <nbespalov@fabricadc.com>
2015-04-25 11:06:15 +0500

    search issues component

--- src/components/Issues.js
+++ src/components/Issues.js
@@ -5,14 +5,14 @@ var React = require('react/addons');
 require('styles/Issues.less');
 
 var Issues = React.createClass({
-  render: function () {
-    return (
-        <div>
-          <p>Content for Issues</p>
-        </div>
-      );
-  }
+    render: function () {
+        return (
+            <form>
+                <input type="text" class="form-control" placeholder="Github user name" />
+            </form>
+        );
+    }
 });
 
-module.exports = Issues; 
+module.exports = Issues;
 
